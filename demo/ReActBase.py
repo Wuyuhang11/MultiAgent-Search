51ecd15fbdc024faf1fce86c8207c92457d97106
Wuyuhang11 <113747888+Wuyuhang11@users.noreply.github.com>
2024-09-27 22:27:56 +0800

    Update ReActBase.py

--- demo/ReActBase.py
+++ demo/ReActBase.py
@@ -5,8 +5,8 @@ from langchain.agents import AgentType
 from langchain_community.llms import Tongyi
 
 # 设置API密钥
-os.environ["SERPAPI_API_KEY"] = "31a3c96f0ce535fd1b43117d3c9298c66753b6dae1da9074708f03ecc8282367"
-os.environ["DASHSCOPE_API_KEY"] = "sk-d07d9d5c4d8d4158abbaf45a40c10042"
+os.environ["SERPAPI_API_KEY"] = "xxxxx"
+os.environ["DASHSCOPE_API_KEY"] = "sk-xxxxxxx"
 
 # 导入所需的库和模型
 DASHSCOPE_API_KEY = "sk-d07d9d5c4d8d4158abbaf45a40c10042"
